683112e6a8b0871352fc6e253f37490c78da0b11
osajide <osajide5@gmail.com>
2022-11-11 18:59:40 +0100

    nullcase

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: osajide <osajide@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 11:20:11 by osajide           #+#    #+#             */
-/*   Updated: 2022/11/07 14:52:22 by osajide          ###   ########.fr       */
+/*   Updated: 2022/11/11 18:58:43 by osajide          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ char *fremainer(char *buffer)
 		j++;
 		i++;
 	}
-	buff[j] = 0;
+	buff[j] = '\0';
 	return (buff);
 }
 
@@ -73,8 +73,6 @@ char *get_next_line(int fd)
 	int i;
 	int readline;
 
-	if (!buffer)
-		buffer = malloc(1);
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return 0;
 	i = 0;
@@ -83,35 +81,33 @@ char *get_next_line(int fd)
 	while(readline > 0 && !ft_strchr(s, '\n'))
 	{
 		readline = read(fd, s, BUFFER_SIZE);
-		//printf("S:%s\n", s);
-		if (readline < 0)
-		{
-			free(s);
-			return (0);
-		}
-		//printf("read:%d\n", readline);
+	//	printf("read: %d\n", readline);
+	//	printf("string readed: %s\n", s);
 		if (readline < 0)
 		{
 			free(s);
 			return (NULL);
 		}
+		else if (readline == 0)
+			return (NULL);
 		s[BUFFER_SIZE] = '\0';
 		//printf("string readed: %s\n", s);
 		buffer = ft_strjoin(buffer, s);
-		// if (ft_strchr(s, '\n'))
-		// 	break;
+	//	printf("Joined buffer: %s\n", buffer);
 		ft_bzero(s, ft_strlen(s));
+	//	printf("s after bzero: %s\n", s);
+		if (ft_strchr(buffer, '\n'))
+			break;
 	}
 	free(s);
 	//printf("BUFFER BEFORE: %s\n", buffer);
 	// if (buffer == NULL)
 	// 	return (NULL);
-	//line = just_line(buffer);
 	line = just_line(buffer);
-	//printf("LINE: %s", line);
-	//printf("BUFFER AFTER: %s\n", buffer);
+	//printf("-----------------------------LINE: %s\n", line);
+//	printf("BUFFER after getting the line: %s\n", buffer);
 	buffer = fremainer(buffer);
-	//printf("BREMAIN: %s\n", buffer);
+//	printf("Buffer remain: %s\n", buffer);
 	return (line);
 }
 
